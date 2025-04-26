fc5ba8e9e7053febc19577b6e0fcdcb09a137f81
Juntong Ye <juy037@ucsd.edu>
2025-04-25 17:43:35 -0700

    debug

--- projects.js
+++ projects.js
@@ -1,6 +1,6 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
-async () => {
+(async () => {
   const projects = await fetchJSON('../lib/projects.json');
 
   const projectsContainer = document.querySelector('.projects');
